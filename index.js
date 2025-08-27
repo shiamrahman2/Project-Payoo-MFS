ff8f321d3e09d40d6b5959e6b0370b54699c3d9c
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-08-27 19:12:22 +0600

    index part complete

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 document.getElementById('loginButton')
   .addEventListener('click',function(event){
     event.preventDefault();
-    console.log('button click');
+    // console.log('button click');
+    const mobileNumber=12345678910;
+    const pinNumber=1234;
+    const inputMobileNumber=parseInt(document.getElementById('mobile-number').value);
+    const inputPinNumber=parseInt(document.getElementById('pin-number').value);
+    // console.log(inputMobileNumber,inputPinNumber);
+
+    if(mobileNumber===inputMobileNumber && pinNumber===inputPinNumber)
+    {
+        window.location.href="./home_page.html"
+    }
+    else
+    {
+        alert('Invalid Input');
+    }
   })
