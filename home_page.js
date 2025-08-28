ff0bd3e04e2b83cf877388e22fb7cdaea325192e
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-08-28 12:09:21 +0600

    complete all home page

--- home_page.js
+++ home_page.js
@@ -1,15 +1,46 @@
 validPin=1234;
 //  add money operation
+
+//  function to get int input value
+
+ function intInputValue(id){
+    const valueInt=parseInt(document.getElementById(id).value);
+    return valueInt;
+ }
+
+ // function to get input value
+
+ function inputValue(id){
+     const valueInput=document.getElementById(id).value;
+     return valueInput;
+ }
+
+//  function to get inner text
+ 
+function getInnerText(id){
+    const innerText=document.getElementById(id).innerText;
+    return innerText;
+}
+
+// function for toggoling
+function getToggle(id){
+     const forms=document.getElementsByClassName('form');
+         for(const form of forms)
+         {
+              form.style.display='none';
+         }
+         document.getElementById(id).style.display='block';
+}
 document.getElementById('add-money-btn')
    .addEventListener('click',function(event){
      event.preventDefault();
-     const bankName=document.getElementById('bank-name').value;
-     const accountNumber=document.getElementById('account-number').value;
-     const addMoney=parseInt(document.getElementById('add-money').value);
-     const pinNumber=parseInt(document.getElementById('pin_number').value);
+     const bankName=inputValue('bank-name');
+     const accountNumber=inputValue('account-number');
+     const addMoney=intInputValue('add-money');
+     const pinNumber=intInputValue('pin_number');
     //  console.log(bankName,accountNumber,addMoney,pinNumber);
 
-    const availableBalance=parseInt(document.getElementById('available-balance').innerText);
+    const availableBalance=parseInt(getInnerText('available-balance'));
       
     if(accountNumber.length<11)
     {
@@ -21,17 +52,21 @@ document.getElementById('add-money-btn')
         alert('please enter a valid pin number');
         return;
     }
-    document.getElementById('available-balance').innerText=availableBalance+addMoney;
+    if (isNaN(addMoney) || addMoney === "") {
+    alert("Input valid amount");
+    return;
+    }
+     document.getElementById('available-balance').innerText=availableBalance+addMoney;
     document.getElementById('add-money').value='';
    })
    // cash out withdraw operation
 document.getElementById('cash-out-money-btn')
   .addEventListener('click',function(event){
     event.preventDefault();
-       const agentNumber=document.getElementById('agent-number').value;
-     const withdrawMoney=parseInt(document.getElementById('cash-out-money').value);
-     const pinNumber=parseInt(document.getElementById('pin-Number').value);
-     const availableBalance=parseInt(document.getElementById('available-balance').innerText);
+       const agentNumber=inputValue('agent-number');
+     const withdrawMoney=intInputValue('cash-out-money');
+     const pinNumber=intInputValue('pin-Number');
+     const availableBalance=intInputValue('available-balance');
 
      if(agentNumber.length<11)
     {
@@ -55,13 +90,28 @@ document.getElementById('cash-out-money-btn')
 
   // togoling cash out and add money
 
-  document.getElementById('add-Money-btn')
+   document.getElementById('add-Money-btn')
      .addEventListener('click',function(){
-         document.getElementById('Cash-Out').style.display='none';
-         document.getElementById('add-Money').style.display='block';
+        getToggle('add-Money');
      })
-document.getElementById('Cash-Out-btn')
+     document.getElementById('Cash-Out-btn')
      .addEventListener('click',function(){
-         document.getElementById('add-Money').style.display='none';
-         document.getElementById('Cash-Out').style.display='block';
+        getToggle('Cash-Out');
      })
+    document.getElementById('transfer-Money-btn')
+     .addEventListener('click',function(){
+        getToggle('Transfer-Money');
+     })
+     document.getElementById('Get-Bonus-btn')
+     .addEventListener('click',function(){
+        getToggle('Get-Bonous');
+     })
+     document.getElementById('pay-bill-btn')
+     .addEventListener('click',function(){
+        getToggle('pay-bill');
+     })
+     document.getElementById('transaction-history-btn')
+     .addEventListener('click',function(){
+        getToggle('transcation-history');
+     })
+    
