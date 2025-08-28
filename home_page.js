64fd6c42bbb92560f37f105adac17c6abcda25e7
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-08-28 13:02:05 +0600

    all complete without transaction section

--- home_page.js
+++ home_page.js
@@ -31,6 +31,20 @@ function getToggle(id){
          }
          document.getElementById(id).style.display='block';
 }
+//function to toggle btn color
+function btntoggle(id){
+    const allOperationalbtns=document.getElementsByClassName('same-btn-class');
+        for(const allOperationalbtn of allOperationalbtns)
+        {
+            allOperationalbtn.classList.remove("border-[#0874f2]","bg-[#0874f20d]")
+        allOperationalbtn.classList.add("border-gray-200")
+        }
+        document.getElementById(id).classList.remove('border-gray-200');
+       document.getElementById(id).classList.add("border-[#0874f2]","bg-[#0874f20d]");
+}
+
+
+
 
 // operation for add money
 
@@ -128,30 +142,91 @@ document.getElementById('cash-out-money-btn')
     document.getElementById('send-money').value='';
     document.getElementById('pin-number-for-send-money').value='';
          })
-  // togoling cash out and add money
+
+//  get Bonous  operation
+
+const bonusCoupn=54321;
+document.getElementById('get-bonus-btn')
+   .addEventListener('click',function(event){
+    event.preventDefault();
+    const inputedCoupon=intInputValue('coupon-number');
+
+    if(inputedCoupon!=bonusCoupn){
+        alert('coupon number is wrong');
+        return;
+    }
+    const availableBalance=parseInt(getInnerText('available-balance'));
+
+      document.getElementById('available-balance').innerText=availableBalance+500;
+      document.getElementById('coupon-number').value='';
+   })
+
+// pay bill operation
+document.getElementById('pay-bill-btn')
+.addEventListener('click',function(event){
+    event.preventDefault();
+
+    const bankName=inputValue('bank-to-pay');
+     const accountNumber=inputValue('account-number-pay-bill');
+     const payMoney=intInputValue('add-money-to-pay');
+     const pinNumber=intInputValue('pin_number_to_pay');
+    //  console.log(bankName,accountNumber,addMoney,pinNumber);
+
+    const availableBalance=parseInt(getInnerText('available-balance'));
+      
+    if(accountNumber.length<11)
+    {
+        alert('please enter a valid account number');
+        return;
+    }
+    if(validPin!=pinNumber)
+    {
+        alert('please enter a valid pin number');
+        return;
+    }
+    if (isNaN(payMoney) || payMoney === "") {
+    alert("Input valid amount");
+    return;
+    }
+     document.getElementById('available-balance').innerText=availableBalance-payMoney;
+    document.getElementById('add-money-to-pay').value='';
+     document.getElementById('pin_number_to_pay').value='';
+})
+
+
+
+
+
+  // togoling 
 
    document.getElementById('add-Money-btn')
      .addEventListener('click',function(){
         getToggle('add-Money');
+        btntoggle('add-Money-btn')
      })
      document.getElementById('Cash-Out-btn')
      .addEventListener('click',function(){
         getToggle('Cash-Out');
+        btntoggle('Cash-Out-btn')
      })
     document.getElementById('transfer-Money-btn')
      .addEventListener('click',function(){
         getToggle('Transfer-Money');
+        btntoggle('transfer-Money-btn')
      })
      document.getElementById('Get-Bonus-btn')
      .addEventListener('click',function(){
         getToggle('Get-Bonous');
+       btntoggle('Get-Bonus-btn')
      })
-     document.getElementById('pay-bill-btn')
+     document.getElementById('pay-Bill-btn')
      .addEventListener('click',function(){
         getToggle('pay-bill');
+           btntoggle('pay-Bill-btn')
      })
      document.getElementById('transaction-history-btn')
      .addEventListener('click',function(){
         getToggle('transcation-history');
+         btntoggle('transaction-history-btn')
      })
     
